9dfe8b9226aab00a897d476320319e060058b053
quoclong01 <ngodacquoclong@gmail.com>
2024-05-25 22:19:38 +0700

    Add env example

--- src/db/config/database.js
+++ src/db/config/database.js
@@ -1,14 +1,14 @@
 module.exports = {
-  host: 'localhost',
-  username: 'postgres',
-  password: 'password1',
-  port: '5433',
-  database: 'postgres',
-  dialect: 'postgres',
+  host: process.env.SQL_HOST,
+  username: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  port: process.env.SQL_PORT,
+  database: process.env.SQL_DATABASE_NAME,
+  dialect: process.env.SQL_DIALECT,
   pool: {
-    max: 4, // max pool size
-    min: 1, // min pool size
-    acquire: 3000, // timeout to wait connection in  milliseconds
-    idle: 10000, // idle time in milliseconds
+    max: Number(process.env.SQL_MAX_POOL), // max pool size
+    min: Number(process.env.SQL_MIN_POOL), // min pool size
+    acquire: Number(process.env.SQL_ACQUIRE_POOL), // timeout to wait connection in  milliseconds
+    idle: Number(process.env.SQL_IDLE_POOL), // idle time in milliseconds
   },
 };
